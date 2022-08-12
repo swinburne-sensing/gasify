259f505f1f1c2a59bd0ec6e9c78441c7e157dd10
Chris <dev@ravngr.com>
2022-08-12 14:02:45 +1000

    Added poetry project configuration

--- experimentdata/__init__.py
+++ experimentdata/__init__.py
@@ -1,13 +1,9 @@
-__version__ = '1.0.0'
+__version__ = '1.2.0'
 
 __author__ = 'Chris Harrison'
 __credits__ = [__author__]
 
 __maintainer__ = 'Chris Harrison'
-__email__ = 'cjharrison@swin.edu.au'
+__email__ = '629204+ravngr@users.noreply.github.com'
 
 __status__ = 'Development'
-
-
-class ExperimentDataError(Exception):
-    pass
